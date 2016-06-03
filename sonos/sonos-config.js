581705240e0755b735f7485c1c17c86bf21ca31f
jm <max.gutchenko@gmail.com>
2016-06-03 14:18:01 +0200

    solving merge issues

--- sonos/sonos-config.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(RED) {
-    function SonosPlayerNode(config) {
-        RED.nodes.createNode(this, config);
-        this.ipaddress = config.ipaddress;
-        this.port      = config.port;
-    }
-
-    RED.nodes.registerType("sonos-config", SonosPlayerNode);
-};
