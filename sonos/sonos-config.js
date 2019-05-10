0710f0999ca70fac72d5a833bb38314a2d6bc0c6
nvision\janvanopstal <van_opstal_jan@telenet.be>
2019-05-10 19:20:00 +0200

    Do not destroy when it's already destoyed?

--- sonos/sonos-config.js
+++ sonos/sonos-config.js
@@ -36,14 +36,14 @@ module.exports = function(RED)
                 });
             })
             .catch((err) => {
-                console.log(err);
+                console.log(err, '<==== deviceDescovery');
                 return;
             });
         });
 
-        setTimeout(function(){
-            search.destroy();
-        }, 5000);
+        // setTimeout(function(){
+            //search.destroy();
+        // }, 5000);
 
   
         //Add a bit of delay for all devices to be discovered
